5607489c22fb365f80b920727cbb3b3df89e22ed
coquellen <coquelleni>
2016-05-30 22:40:24 +0200

    New email adress

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
 	version       =   "0.0.1",
 	description   =   "NanoPeakCell serial crystallography package",
 	author        =   "Nicolas Coquelle",
-	author_email  =   "coquelle@ibs.fr",
+	author_email  =   "coquelleni@ill.fr",
 	packages      =   find_packages(),
-    package_data={'NPC': ['bitmaps/*','gui/cmaps/*']},
+        package_data={'NPC': ['bitmaps/*','gui/cmaps/*']},
 	scripts = ['NPC/npc','NPC/gui/npg'],
 
 )
